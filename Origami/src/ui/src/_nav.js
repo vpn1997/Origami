ff5478baf5b88623ac7a5a1debf534d4dada47f1
vpn1997 <mailme.vipin475@gmail.com>
2018-06-14 17:15:27 +0530

    added ui

---
+++ Origami/src/ui/src/_nav.js
@@ -0,0 +1,38 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/home',
+      icon: 'icon-home'
+
+    },
+      {
+      name: 'My Demo',
+      url: '/ngh/user/',
+      icon: 'icon-home'
+
+      },      
+      {
+        name: 'Create demo',
+        url: '/ngh/user/register',
+        icon: 'icon-home'
+
+      },
+      {
+        name: 'Help',
+        url: '/ngh/user/register',
+        icon: 'icon-home'
+
+      },
+      {
+        name: 'Logout',
+        url: '/ngh/user/register',
+        icon: 'icon-home'
+
+      },
+
+
+
+    
+  ],
+};
